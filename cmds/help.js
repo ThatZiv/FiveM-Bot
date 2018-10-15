ee393f6f0f7876cca126f5b8438fe1ffb0832c32
Zavaar Shah <35277113+ThatZiv@users.noreply.github.com>
2018-10-14 23:57:27 -0400

    Update help.js

--- cmds/help.js
+++ cmds/help.js
@@ -1,8 +1,6 @@
 exports.run = async (client, message, args) => {
     var util = require("../fivem.js")
     message.delete();
-    req(`${config.cdn}:9999/api`, function (err, response, body) {
-    if (err) {
     let embed = new Discord.RichEmbed()
     .setAuthor(`${client.user.username} Bot on ${client.guilds.size} Servers`, client.user.avatarURL)
     .setColor(color)
@@ -13,7 +11,7 @@ exports.run = async (client, message, args) => {
     .addField(`${config.prefix}find <username>`, "Finds a user that is on the server via Username")
     .addField(`${config.prefix}id <id>`, "Finds a user that is on the server via Server ID")
     .addBlankField(true)
-    .addField("Global FiveM Bot Log", `${config.cdn}:1234`)
+    .addField("FiveM Bot API", `Log - ${config.cdn}:1234\nAPI (JSON) - ${config.cdn}:1234/api`)
     .setThumbnail(`${config.cdn}/assets/fivem.png`)
     .setTitle("Support Discord")
     .setURL("https://discord.gg/yWddFpQ")
@@ -22,29 +20,5 @@ exports.run = async (client, message, args) => {
     .setFooter("Copyright © Zua - http://zavaar.cf")
     console.log(`FIVEM help cmd was used.. in ${message.guild.name}`)
     util.log(`FIVEM help cmd was used`)
-    message.channel.send({embed: embed});
-    }
-    var res = JSON.parse(body)
-    let embed = new Discord.RichEmbed()
-    .setAuthor(`${client.user.username} Bot on ${client.guilds.size} Servers`, client.user.avatarURL)
-    .setColor(color)
-    .setDescription(`**__FiveM Bot Help__**`)
-    .addField(`${config.prefix}set`, "Sets the current guild FiveM server")
-    .addField(`${config.prefix}players`, "Gets the current player list of a server")
-    .addField(`${config.prefix}server`, "Gets all information of a server")
-    .addField(`${config.prefix}find <username>`, "Finds a user that is on the server")
-    .addField(`${config.prefix}id <id>`, "Finds a user that is on the server via Server ID")
-    .addBlankField(true)
-    .addField("Global FiveM Bot Log", `${config.cdn}:1234`)
-    .setThumbnail(`${config.cdn}/assets/fivem.png`)
-    .setTitle("Support Discord")
-    .setURL("https://discord.gg/yWddFpQ")
-    .addBlankField(true)
-    .addField("Also Remember To Check Out Zua's:", `Twitch - https://twitch.tv/zuacp \nYoutube - https://www.youtube.com/channel/UCjAu7aRli_GePQb4FnzRRWg\nWebsite - http://zavaar.cf/\n\n**${res.title}**\n${res.text}\n`)
-    .setFooter("Copyright © Zua - http://zavaar.cf")
-     console.log(`FIVEM help cmd was used.. in ${message.guild.name}`)
-    util.log(`Help`)
-    message.channel.send({embed: embed});
-
-})
+    message.channel.send({embed: embed})
 };
