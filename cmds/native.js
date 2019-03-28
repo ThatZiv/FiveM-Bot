b1f31c4a7f415ef0707b769efa406a69c0022326
Zavaar Shah <35277113+ThatZiv@users.noreply.github.com>
2019-03-28 19:50:08 -0400

    added native

---
+++ cmds/native.js
@@ -0,0 +1,44 @@
+exports.run = async (client, message, args) => {
+    var util = require("../fivem.js");
+      /* ----------------------------- */ 
+      
+      // decided not to use l8er
+      let query = []
+      var searchQuery = args.join(" ")
+      var options = {
+          shouldSort: true,
+          findAllMatches: true,
+          includeMatches: true,
+          threshold: 0.8,
+          location: 0,
+          distance: 100,
+          maxPatternLength: 32,
+          minMatchCharLength: 1,
+        };
+        try {
+        var fuse = new Fuse(listNatives, options); // "listNatives" is the item array
+        var result = fuse.search(searchQuery);
+        //console.log(result[0].matches[0].value)
+
+      for(i=0;i<tempNatives.length;i++){
+          if (tempNatives[i].body == result[0].matches[0].value) {
+              query.push(tempNatives[i].prefix)
+              let embed = new Discord.RichEmbed()
+                .setAuthor(state, icon)
+                .setDescription(tempNatives[i].body)
+                .addField("Body:", `\`\`\`yaml\n${tempNatives[i].prefix}\`\`\``)
+                .addField("Description", tempNatives[i].description)
+                .setColor(color)
+                .setThumbnail(icon)
+                .setFooter(`FiveM Native Search: ${searchQuery}`)
+              message.channel.send({ embed: embed })
+              return;
+          }
+      }
+
+      
+
+    } catch(err) {
+      util.zembed(`Unable to find the native \`${searchQuery}\`.`);
+    }
+};
