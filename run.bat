0e293b01e9946d6bd388441bc45802aa5ffcff6b
unknown <thatziv@gmail.com>
2019-05-17 22:02:29 -0400

    cleaning

--- run.bat
+++
@@ -1,1 +0,0 @@
-node fivem.js
--- shard.js
+++
@@ -1,4 +0,0 @@
-const Discord = require("discord.js")
-const Manager = new Discord.ShardingManager('./fivem.js');
-Manager.spawn(2); 
-// This example will spawn 2 shards (5,000 guilds);
